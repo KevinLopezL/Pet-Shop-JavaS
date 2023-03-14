f89af449ef45b890ca780b28e45499d398519a13
Kevin Lopez <kevinlopeznom@gmail.com>
2023-03-14 10:08:38 -0300

    actualizando

--- Js/main.js
+++ Js/main.js
@@ -189,7 +189,39 @@ function cargarProductosCarrito(array){
 }
 
 function finalizarCompra (array){
-    console.log 
+    console.log  ("funciona")
+    
+        Swal.fire({
+            title: 'Está seguro de realizar la compra?',
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonText: 'Sí, seguro',
+            cancelButtonText: 'No, no quiero',
+            confirmButtonColor: 'green',
+            cancelButtonColor: 'red',
+        }).then((result)=>{
+            if(result.isConfirmed){
+                Swal.fire({
+                    title: 'Compra realizada',
+                    icon: 'success',
+                    confirmButtonColor: 'green',
+                    text: `Muchas gracias por su compra `,
+                    })
+                    productosEnCarrito = []
+                    localStorage.removeItem("carrito")
+            }else{
+                Swal.fire({
+                    title: 'Compra no realizada',
+                    icon: 'info',
+                    text: `La compra no ha sido realizada! Atención sus productos siguen en el carrito`,
+                    confirmButtonColor: 'green',
+                    timer:3500
+                })
+            }
+        }
+    
+        )
+    
 }
 
 
